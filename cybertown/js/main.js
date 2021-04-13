c8eab4dee38f502a8e60068ac04765c24a45b0a7
Rubenstein Ilya <vasilijpupkin6101@gmail.com>
2021-04-13 22:39:08 +0300

    commit cybertown

--- cybertown/js/main.js
+++ cybertown/js/main.js
@@ -36,3 +36,17 @@ for (i = 0; i < acc.length; i++) {
 document.querySelector('.burger').onclick = function () {
     document.querySelector('.list').classList.toggle('show')
 }
+
+// scroll
+
+const progress = document.querySelector('.progress');
+ 
+window.addEventListener('scroll', progressBar);
+
+function progressBar(e) {
+  let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let per = windowScroll / windowHeight * 100;
+
+  progress.style.width = per + '%';
+}
