cc9be9bb8a35ef8d96edb4aa2ad25a0b6392b98b
Rubenstein Ilya <vasilijpupkin6101@gmail.com>
2021-04-20 15:19:10 +0300

    yeas

--- cybertown/js/main.js
+++ cybertown/js/main.js
@@ -1,52 +1,15 @@
-const cards = document.querySelectorAll('.card');
+// burger
+$(document).ready(function () {
+  $('.burger').click(function (){
+    $('.list').toggleClass('show')
+  })
+})
 
-for (let i = 0; i < cards.length; i++) {
-    const card = cards[i];
-    card.addEventListener('mousemove', startRotate);
-    card.addEventListener('mouseout', stopRotate);
-}
-
-function startRotate(event) {
-    const cardItem = this.querySelector('.card-item');
-    const halfHeight = cardItem.offsetHeight / 2;
-    console.log(halfHeight);
-    cardItem.style.transform = 'rotateX('+-(event.offsetY - halfHeight)/5+'deg) rotateY('+ (event.offsetX - halfHeight) /5+'deg)'
-};
-
-function stopRotate(event) {
-    const cardItem = this.querySelector('.card-item');
-    cardItem.style.transform = 'rotate(0)';
-};
 
 // acc
 
-var acc = document.getElementsByClassName('btn-eye');
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    this.classList.toggle('active');
-
-    this.nextElementSibling.classList.toggle('show-p')
-  }
-}
-
-// burger
-
-document.querySelector('.burger').onclick = function () {
-    document.querySelector('.list').classList.toggle('show')
-}
-
-// scroll
-
-const progress = document.querySelector('.progress');
- 
-window.addEventListener('scroll', progressBar);
-
-function progressBar(e) {
-  let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  let per = windowScroll / windowHeight * 100;
-
-  progress.style.width = per + '%';
-}
+$(document).ready(function() {
+  $('.btn-eye').click(function() {
+    $(this).next('.eye-p').slideToggle()
+  })
+})
--- evklid/js/script.js
+++ evklid/js/script.js
@@ -4,10 +4,6 @@ document.querySelector('.header__link-burger').onclick = function () {
   document.querySelector('.header__nav').classList.toggle('show')
 };
 
-document.querySelector('.burger-close').onclick = function () {
-  document.querySelector('.show').classList.remove('show');
-};
-
 // accordion
 
 var acc = document.getElementsByClassName('section__questions-item');
@@ -19,4 +15,36 @@ for (i = 0; i < acc.length; i++) {
 
     this.nextElementSibling.classList.toggle('show-p')
   }
+}
+
+// tab
+
+var tab;
+var tabContent;
+
+window.onload = function () {
+    tabContent = document.getElementsByClassName('section__work__block');
+    tab = document.getElementsByClassName('section__work-link');
+    hideTabsContent(1);
+};
+
+function hideTabsContent(a) {
+    for (let i = a; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+        tab[i].classList.remove('active');
+    }
+}
+
+document.getElementById('section__work__block').onclick = function (event) {
+    var target = event.target;
+    if (target.className=='section__work-link') {
+        for (let i = 0; i < tab.length; i++) {
+            if (target == tab[i]) {
+                showTabsContent(i);
+                break;
+            }
+
+        }
+    }
 }
