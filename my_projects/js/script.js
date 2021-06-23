c14edc3475cd1858c01480d2374bf59648286665
Rubenstein Ilya <vasilijpupkin6101@gmail.com>
2021-06-23 22:33:38 +0300

    commit delete nedo-projects

--- my_projects/js/script.js
+++
@@ -1,9 +0,0 @@
-// var burgerBtn = document.querySelector('.burger');
-// burgerBtn.addEventListener('click', open);
-// function open() {
-//     var menu = document.getElementsByClassName('header__nav-list').classList.toggle('show');
-// }
-
-document.querySelector('.burger').onclick = function() {
-    document.querySelector('.header__nav-list').classList.toggle('show')
-}
--- spacex/script/script.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    $('.header__nav-burger').click(function(event) {
-        $('.header__nav-burger, .header__nav').togleClass('active');
-});
