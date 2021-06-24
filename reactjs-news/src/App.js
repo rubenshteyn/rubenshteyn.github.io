2c3e6802725775d9cc0aaa2a7d856239ad9639a6
Rubenstein Ilya <vasilijpupkin6101@gmail.com>
2021-06-24 23:01:24 +0300

    projects REACTJS

---
+++ reactjs-news/src/App.js
@@ -0,0 +1,64 @@
+  
+import React from 'react'
+import { Add } from './components/Add'
+import { News } from './components/News'
+import './css/app.css'
+import './css/normalize.css'
+
+class App extends React.Component {
+  state = {
+    news: null,
+    isLoading: false,
+  }
+  static getDerivedStateFromProps(props, state) {
+    let nextFilteredNews
+
+    // смотрим в state.news (ранее смотрели в props)
+    // и проверяем, чтобы не клоинировать null
+    // например, в момент первой отрисовки
+    if (Array.isArray(state.news)) {
+      nextFilteredNews = [...state.news]
+
+      nextFilteredNews.forEach((item, index) => {
+        if (item.bigText.toLowerCase().indexOf('pubg') !== -1) {
+          item.bigText = 'СПАМ'
+        }
+      })
+
+      return {
+        filteredNews: nextFilteredNews,
+      }
+    }
+
+    return null
+  }
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    fetch('http://localhost:3000/data/newsData.json')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setTimeout(() => {
+          this.setState({ isLoading: false, news: data })
+        }, 1000)
+      })
+  }
+  handleAddNews = data => {
+    const nextNews = [data, ...this.state.news]
+    this.setState({ news: nextNews })
+  }
+  render() {
+    const { news, isLoading } = this.state
+
+    return (
+      <React.Fragment>
+        <Add onAddNews={this.handleAddNews} />
+        {isLoading && <p>Загружаю...</p>}
+        {Array.isArray(news) && <News data={news} />}
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
